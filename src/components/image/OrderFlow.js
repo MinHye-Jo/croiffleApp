ca3cf8e5b9057d0977871d95401be77fabf4fd13
조민혜 <laura.jo@jeonbotdae.com>
2021-07-13 15:04:51 +0900

    수정

--- src/components/image/OrderFlow.js
+++ src/components/image/OrderFlow.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
 
-const OrderFlow = () => {
+const OrderFlow = ({ orderStatus }) => {
 
   const styles = StyleSheet.create({
     container: {
@@ -28,30 +28,47 @@ const OrderFlow = () => {
       marginTop: 10
     }
   });
+  console.log(orderStatus)
+  const img1 = orderStatus == "1" ?
+    require('../../../assets/image/icon/icon_step1_b.png') :
+    require('../../../assets/image/icon/icon_step1_g.png');
+
+  const img2 = orderStatus == "2" ?
+    require('../../../assets/image/icon/icon_step2_b.png') :
+    require('../../../assets/image/icon/icon_step2_g.png');
+
+  const img3 = orderStatus == "3" ?
+    require('../../../assets/image/icon/icon_step3_b.png') :
+    require('../../../assets/image/icon/icon_step3_g.png');
+
+  const img4 = orderStatus == "4" || orderStatus == "5" ?
+    require('../../../assets/image/icon/icon_step4_b.png') :
+    require('../../../assets/image/icon/icon_step4_g.png');
+
 
   return (
     <View style={styles.container}>
       <View style={styles.viewContainer}>
-        <Image source={require('../../../assets/image/icon/icon_step1_b.png')} style={{ height: 35, resizeMode: 'contain' }} />
-        <Text style={styles.activeTxt}>주문요청중</Text>
+        <Image source={img1} style={{ height: 35, resizeMode: 'contain' }} />
+        <Text style={orderStatus == "1" ? styles.activeTxt : styles.txt}>주문요청중</Text>
       </View>
       <Image source={require('../../../assets/image/icon/icon_next_g.png')} style={{ width: 25, resizeMode: 'contain' }} />
 
       <View style={styles.viewContainer}>
-        <Image source={require('../../../assets/image/icon/icon_step2_g.png')} style={{ height: 35, resizeMode: 'contain' }} />
-        <Text style={styles.txt}>준비중</Text>
+        <Image source={img2} style={{ height: 35, resizeMode: 'contain' }} />
+        <Text style={orderStatus == "2" ? styles.activeTxt : styles.txt}>준비중</Text>
       </View>
       <Image source={require('../../../assets/image/icon/icon_next_g.png')} style={{ width: 25, resizeMode: 'contain' }} />
 
       <View style={styles.viewContainer}>
-        <Image source={require('../../../assets/image/icon/icon_step2_g.png')} style={{ height: 35, resizeMode: 'contain' }} />
-        <Text style={styles.txt}>준비완료</Text>
+        <Image source={img3} style={{ height: 35, resizeMode: 'contain' }} />
+        <Text style={orderStatus == "3" ? styles.activeTxt : styles.txt}>준비완료</Text>
       </View>
       <Image source={require('../../../assets/image/icon/icon_next_g.png')} style={{ width: 25, resizeMode: 'contain' }} />
 
       <View style={styles.viewContainer}>
-        <Image source={require('../../../assets/image/icon/icon_step4_g.png')} style={{ height: 35, resizeMode: 'contain' }} />
-        <Text style={styles.txt}>픽업완료</Text>
+        <Image source={img4} style={{ height: 35, resizeMode: 'contain' }} />
+        <Text style={orderStatus == "4" || orderStatus == "5" ? styles.activeTxt : styles.txt}>픽업완료</Text>
       </View>
     </View>
   );
