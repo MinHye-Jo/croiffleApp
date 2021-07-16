635e75326f6c812061e243da84435efa15ad7446
조민혜 <laura.jo@jeonbotdae.com>
2021-07-16 18:06:51 +0900

    사운드 플레이 완료

--- android/app/src/main/java/com/croifflekitchen/employee/MainApplication.java
+++ android/app/src/main/java/com/croifflekitchen/employee/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactInstanceManager;
