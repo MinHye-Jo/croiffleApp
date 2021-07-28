5d0e3d3f19dc250ed9833bc97bfd63a32225ad70
조민혜 <laura.jo@jeonbotdae.com>
2021-07-28 17:24:56 +0900

    헤더 사이즈 변경

--- src/screens/header/PrevHeaderModule.js
+++ src/screens/header/PrevHeaderModule.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Image, View, TouchableOpacity, Text } from 'react-native';
 import { Header } from 'react-native-elements';
 import styles from 'styles/commonStyle';
+import DeviceInfo from 'react-native-device-info';
+
+const osType = DeviceInfo.getSystemName() === 'Android' ? 'ANDROID' : 'iOS';
 
 // 이전페이지
 const renderPrevBtn = props => (
@@ -25,7 +28,7 @@ const renderCenterTitle = props => (
 const PrevHeaderModule = props => {
   return (
     <Header
-      containerStyle={{ height: 100 }}
+      containerStyle={{ height: osType == 'iOS' ? 100 : 80 }}
       backgroundColor="rgb(0, 191, 213)"
       placement="center"
       leftComponent={renderPrevBtn(props)}
