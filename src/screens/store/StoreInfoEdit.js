8ea3fb23751ee00288ecea9d48ed3a97bf417dc3
조민혜 <laura.jo@jeonbotdae.com>
2021-07-30 14:20:11 +0900

    핸드폰번호

--- src/screens/store/StoreInfoEdit.js
+++ src/screens/store/StoreInfoEdit.js
@@ -18,10 +18,10 @@ const StoreInfoEdit = ({ route }) => {
     addressRoad: '',
     addressDetail: '',
     postalCode: '',
-    phone: ''
+    phone: '',
   });
 
-  const [fileName, setFileName] = useState('')
+  const [fileName, setFileName] = useState('');
 
   // 모달 데이터
   const [postModalOpen, setPostModalOpen] = useState(false);
@@ -34,36 +34,41 @@ const StoreInfoEdit = ({ route }) => {
 
     if (data.return_code == 200) {
       setShopInfo({
-        ...(Object.keys(shopInfo).reduce((obj, key) => {
-          if (key == "file") setFileName(data.response.shopImgUrl);
-          else obj[key] = data.response[key];
+        ...Object.keys(shopInfo).reduce((obj, key) => {
+          if (key == 'file') {
+            setFileName(data.response.shopImgUrl);
+          } else if (key == 'phone') {
+            obj[key] = data.response[key].replace(/(\d{3})(-*)(\d{4})(-*)(\d{4})/, '$1-$3-$5');
+          } else {
+            obj[key] = data.response[key];
+          }
           return obj;
-        }, {}))
+        }, {}),
       });
     } else {
       setModalOpen(true);
-      setModalText(data.return_message)
+      setModalText(data.return_message);
     }
   }, []);
 
   // 입력 데이터 셋
   const updateInput = (key, value) => {
-    if (key == "address") {
+    if (key == 'address') {
       setShopInfo({
         ...shopInfo,
         addressRoad: value.address,
-        postalCode: value.zonecode
-      })
+        postalCode: value.zonecode,
+      });
 
       // 우편번호 검색 모달 닫기
       setPostModalOpen(false);
     } else {
       setShopInfo({
         ...shopInfo,
-        [key]: value
-      })
+        [key]: value,
+      });
     }
-  }
+  };
 
   // 이미지 파일 업로드
   const selectImage = () => {
@@ -74,53 +79,63 @@ const StoreInfoEdit = ({ route }) => {
       },
     };
 
-    launchImageLibrary(options, (response) => {
+    launchImageLibrary(options, response => {
       if (response.error) {
         setModalOpen(true);
-        setModalText("이미지 업로드에 실패하였습니다.");
+        setModalText('이미지 업로드에 실패하였습니다.');
       } else {
         const resData = response.assets[0];
         setFileName(resData.fileName);
         updateInput('file', {
           name: resData.fileName,
           type: resData.type,
-          uri: Platform.OS === 'ios' ? resData.uri.replace('file://', '') : resData.uri
-        })
+          uri: Platform.OS === 'ios' ? resData.uri.replace('file://', '') : resData.uri,
+        });
       }
     });
-  }
+  };
 
   // 매장 정보 수정 api
   const shopEditApi = async () => {
     const { data } = await shopEdit(shopInfo);
     if (data.return_code == 200) {
       setModalOpen(true);
-      setModalTitle("매장정보 수정");
-      setModalText("저장 되었습니다.")
+      setModalTitle('매장정보 수정');
+      setModalText('저장 되었습니다.');
     } else {
       setModalOpen(true);
-      setModalTitle("매장정보 수정실패");
-      setModalText(data.return_message)
+      setModalTitle('매장정보 수정실패');
+      setModalText(data.return_message);
     }
-  }
+  };
 
   return (
     <ScrollView style={styles.topContainer}>
+      <DefaultModal
+        modalOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        title={modalTitle}
+        modalText={modalText}
+      />
 
-      <DefaultModal modalOpen={modalOpen} onClose={() => setModalOpen(false)} title={modalTitle} modalText={modalText} />
-
-      <FindPostCode modalOpen={postModalOpen}
+      <FindPostCode
+        modalOpen={postModalOpen}
         onClose={() => setPostModalOpen(false)}
-        onSelect={(data) => updateInput('address', data)} />
+        onSelect={data => updateInput('address', data)}
+      />
 
       <View style={{ paddingLeft: 20, paddingRight: 20 }}>
         <Text style={{ ...styles.font5M15, marginTop: 30, marginBottom: 10 }}> 매장명 </Text>
-        <TextInput style={{ ...styles.greyInput }} value={shopInfo.shopName}
-          onChangeText={e => updateInput('shopName', e)} />
+        <TextInput
+          style={{ ...styles.greyInput }}
+          value={shopInfo.shopName}
+          onChangeText={e => updateInput('shopName', e)}
+        />
 
         <Text style={{ ...styles.font5M15, marginTop: 30, marginBottom: 10 }}> 매장 사진등록 </Text>
         <View style={styles.row}>
-          <TextInput style={{ ...styles.greyInput, flex: 1.5 }}
+          <TextInput
+            style={{ ...styles.greyInput, flex: 1.5 }}
             placeholder="파일을 첨부해주세요"
             placeholderTextColor="rgb(174, 174, 174)"
             value={fileName}
@@ -133,12 +148,18 @@ const StoreInfoEdit = ({ route }) => {
         <Text style={{ ...styles.font5M15, marginTop: 30, marginBottom: 10 }}> 매장주소 </Text>
         <View style={styles.row}>
           <TextInput style={{ ...styles.greyInput, flex: 1.5 }} editable={false} value={shopInfo.postalCode} />
-          <TouchableOpacity style={{ ...styles.blueBtn, flex: 1, marginLeft: 10 }} onPress={() => setPostModalOpen(true)}>
+          <TouchableOpacity
+            style={{ ...styles.blueBtn, flex: 1, marginLeft: 10 }}
+            onPress={() => setPostModalOpen(true)}>
             <Text style={styles.btnTxtWhite}>우편번호 검색</Text>
           </TouchableOpacity>
         </View>
         <TextInput style={{ ...styles.greyInput, marginTop: 10 }} editable={false} value={shopInfo.addressRoad} />
-        <TextInput style={{ ...styles.greyInput, marginTop: 10 }} value={shopInfo.addressDetail} onChangeText={e => updateInput('addressDetail', e)} />
+        <TextInput
+          style={{ ...styles.greyInput, marginTop: 10 }}
+          value={shopInfo.addressDetail}
+          onChangeText={e => updateInput('addressDetail', e)}
+        />
 
         <Text style={{ ...styles.font5M15, marginTop: 30, marginBottom: 10 }}> 매장 전화번호 </Text>
         <TextInput style={{ ...styles.greyInput }} value={shopInfo.phone} onChangeText={e => updateInput('phone', e)} />
@@ -148,7 +169,7 @@ const StoreInfoEdit = ({ route }) => {
             <Text style={styles.btnTxtWhite}>저장</Text>
           </View>
         </TouchableOpacity>
-      </View >
+      </View>
     </ScrollView>
   );
 };
